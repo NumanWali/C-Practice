f0a57fe60783b7bbdac8d87df06aaba7c230b44e
NumanWali <numanwali08182@gmail.com>
2024-07-24 21:56:55 +0500

    Add files via upload

---
+++ simple_interest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float p = 540;
+  float r = 32;
+  float t = 3;
+  float si = (p*r*t/100); //and there you can give any type of value to fing simple interest.
+  cout<<si;
+}
