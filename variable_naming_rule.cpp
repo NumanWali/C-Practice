bb6923e463d5cf0e011c319e39bb3d386d821447
NumanWali <numanwali08182@gmail.com>
2024-07-27 18:00:10 +0500

    Add files via upload
    this is how to give name of the variables.

---
+++ variable_naming_rule.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int FLOAT= 3;
+  cout<<FLOAT<<endl; //indeed float is reserved word but here we write cappital .
+  int $=6;
+  cout<<$<<endl; // here we start form underscore and $ only this special charactor
+  // all other special charactors are not allowed.
+}
