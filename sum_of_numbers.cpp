4a205c47193cd2a7293a3cd973b9c241de8fc74f
NumanWali <numanwali08182@gmail.com>
2024-07-27 18:01:27 +0500

    Add files via upload
    sum of numbers.

---
+++ sum_of_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float x,y,z;
+  cout<<"enter first number: ";
+  cin>>x;
+  cout<<"enter second number: ";
+  cin>>y;
+  cout<<"enter third  number: ";
+  cin>>z;
+  cout<<"the sum of given numbers is = ";
+   cout<<x+y+z;
+  
+}
