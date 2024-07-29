b6a4934357c493dcf8fd1f1a7f3eafa648911a06
NumanWali <numanwali08182@gmail.com>
2024-07-29 15:56:44 +0500

    Add files via upload

---
+++ modulus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+  int a = 8;
+  int b = 3;
+  cout<<a%b<<endl;
+  int c = 10;
+  cout<<c%c<<endl;
+  int d = 15;
+  int e = 4;
+  cout<<d%(-e)<<endl;
+  int f = 20;
+  int g = 7;
+  cout<<(-f)%g;
+}
