c93ecad981c36babf20658ba26543e752173e1a3
NumanWali <numanwali08182@gmail.com>
2024-08-02 15:31:43 +0500

    Add files via upload
    This code is for even or odd by if and else condition. in this code input from the user and then give the answer ( even or odd)

---
+++ find_even_or_odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int  main(){
+int n;
+cout<<"enter a number: ";
+cin>>n;
+if (n%2==0 ) {
+   cout<<"this is even number"<<endl;
+   cout<<"wow";
+}
+
+else {
+   cout<<"this is odd number"<<endl;
+  cout<<"wow";
+}
+}
