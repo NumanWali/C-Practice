9fbe2b57cee0b75bf3e4130f9ef979532772cd9c
NumanWali <numanwali08182@gmail.com>
2024-08-18 16:19:14 +0500

    Add files via upload

---
+++ ternary_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int  main(){
+int n;
+cout<<"enter a number: ";
+cin>>n;
+(n%2==0)? cout<<"even": cout<<"odd"; //this is called ternary operator only replacement of if-else.
+                                 // it minimize the code for the user
+                                 // it is only applicable for the two condition
+}
