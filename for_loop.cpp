d1334972ef09eabccbfef7e82247eb16f13817da
NumanWali <numanwali08182@gmail.com>
2024-08-18 16:40:11 +0500

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // int n;
+  // cout<<"Enter the number: ";
+  // cin>>n;
+  for(int i=1; i<=7; i++){
+    cout<<"my name is Numan wali marwat"<<endl;
+  }
+}
