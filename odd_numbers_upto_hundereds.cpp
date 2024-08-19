6ad08c6bfb6080dc99a522a9c064c941ae25e4cd
NumanWali <numanwali08182@gmail.com>
2024-08-19 17:53:51 +0500

    Add files via upload

---
+++ odd_numbers_upto_hundereds.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  for(int i=1; i<=100; i++)
+  {
+    if(i%2!=0){
+cout<<i<<" ";
+    }
+    
+  }
+}
+//  secong method 
+// for( int i=1; i<=100; i=i+2){
+//   cout<<i<<"";
+// }
