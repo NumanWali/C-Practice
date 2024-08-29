5807f9e283a010001f047dea6b7fa3255943b418
NumanWali <numanwali08182@gmail.com>
2024-08-29 12:44:36 +0500

    Add files via upload

---
+++ continue_statment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // int n;
+  // cout<<"enter the number: ";
+  // cin>>n;
+
+for(int i=1; i<=20; i++){
+  if (i==3) continue;// continue means to skip a round 
+  if( i==8 ) continue;
+    cout<<i<<" ";  
+   
+}
+}
