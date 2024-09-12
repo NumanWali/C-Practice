739b42a1e3606dd19ae6dea5a46c8bc355837742
NumanWali <numanwali08182@gmail.com>
2024-09-12 09:44:57 +0500

    Add files via upload

---
+++ libray_function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter the number:";
+  cin>>n;
+  cout<<sqrt(n); // library function   // here int means input
+
+}
