a33086fc3b6795c331bf1dcb42b0453b02d14d4f
NumanWali <numanwali08182@gmail.com>
2024-09-12 09:44:18 +0500

    Add files via upload

---
+++ Basic_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void usa(){    //here  you make unlimated fuction and call it unlimated.
+  cout<<"you are in usa"<<endl;
+  return; //it means that the fuction has been finish.
+  cout<<"you are in usa"<<endl; // it can not be printed.
+}
+void pakistan(){
+    cout<<"you are in pakistan"<<endl;
+}
+int main(){
+
+cout<<"how ar yoy"<<endl;
+  usa();
+  pakistan();
+}
