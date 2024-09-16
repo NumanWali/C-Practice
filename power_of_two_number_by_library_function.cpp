ad478f8046e80f3fa92cb34f4e82405b130802f3
NumanWali <numanwali08182@gmail.com>
2024-09-16 23:31:51 +0500

    Add files via upload

---
+++ power_of_two_number_by_library_function.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<cmath>    //library fuction header file
+using namespace std;
+int main(){
+  cout<<pow(3,4);     //here the first number is base and second one is power. 
+}
