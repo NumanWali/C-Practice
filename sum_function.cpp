7c32e491a1d41fe7ae66e8635698d865da470d78
NumanWali <numanwali08182@gmail.com>
2024-09-18 21:52:08 +0500

    Add files via upload

---
+++ sum_function.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std;
+void sum(int a, int b, int c){ //here int a and int b are formal parameter
+   cout<<a+b+c;
+}
+int main(){
+  sum(7,12,34);  // here 7,12 are actuall parameters
+}                // formal parameter=actuall paramter.
